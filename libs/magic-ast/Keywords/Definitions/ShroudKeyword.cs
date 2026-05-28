5064404e7134739340415a34db703d7573bb2653
Spencer Elkington <spelkington@gmail.com>
2026-05-28 01:43:17 -0600

    refactor(mast): migrate keyword batch P2B-14 to Definitions/

---
+++ libs/magic-ast/Keywords/Definitions/ShroudKeyword.cs
@@ -0,0 +1,33 @@
+namespace MagicAST.Keywords.Definitions;
+
+using MagicAST.AST.Abilities;
+using MagicAST.AST.Effects.Keyword;
+using MagicAST.Parsing.Tokens;
+using Superpower;
+using static MagicAST.Keywords.Definitions.KeywordCombinators;
+
+/// <summary>
+/// Shroud: This permanent can't be the target of spells or abilities (including
+/// by its controller). Rule 702.18. MAST records keyword presence.
+/// </summary>
+[Keyword]
+public sealed class ShroudKeyword : IKeyword
+{
+  /// <inheritdoc/>
+  public KeywordTier Tier => KeywordTier.Simple;
+
+  /// <inheritdoc/>
+  public KeywordDefinition? Definition => null;
+
+  /// <inheritdoc/>
+  public TokenListParser<OracleToken, StaticAbility> Combinator { get; } = (
+    from keyword in Keyword("Shroud")
+    from reminder in OptionalReminder
+    select new StaticAbility
+    {
+      KeywordSource = "Shroud",
+      Effects = [new ShroudEffect()],
+      Reminder = reminder,
+    }
+  );
+}
