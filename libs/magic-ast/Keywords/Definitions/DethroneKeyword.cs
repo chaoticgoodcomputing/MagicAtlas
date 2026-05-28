22045e7844efa3643990fd615652742b737d0415
Spencer Elkington <spelkington@gmail.com>
2026-05-28 01:43:00 -0600

    refactor(mast): migrate keyword batch P2B-4 to Definitions/

---
+++ libs/magic-ast/Keywords/Definitions/DethroneKeyword.cs
@@ -0,0 +1,46 @@
+namespace MagicAST.Keywords.Definitions;
+
+using MagicAST.AST.Abilities;
+using MagicAST.AST.Effects.Keyword;
+using MagicAST.Parsing.Tokens;
+using Superpower;
+using static MagicAST.Keywords.Definitions.KeywordCombinators;
+
+/// <summary>
+/// Dethrone: Whenever this creature attacks the player with the most life or
+/// tied for most life, put a +1/+1 counter on it.
+/// Rule 702.107. Parameterless keyword marker.
+/// </summary>
+[Keyword]
+public sealed class DethroneKeyword : IKeyword
+{
+  /// <inheritdoc/>
+  public KeywordTier Tier => KeywordTier.Simple;
+
+  /// <inheritdoc/>
+  public KeywordDefinition Definition { get; } =
+    new()
+    {
+      Name = "Dethrone",
+      RuleReference = "702.107",
+      Category = KeywordCategory.Triggered,
+      HasParameter = false,
+      CreateExpansion = _ => new StaticAbility
+      {
+        KeywordSource = "Dethrone",
+        Effects = [new DethroneEffect()],
+      },
+    };
+
+  /// <inheritdoc/>
+  public TokenListParser<OracleToken, StaticAbility> Combinator { get; } = (
+    from kw in Keyword("Dethrone")
+    from reminder in OptionalReminder
+    select new StaticAbility
+    {
+      KeywordSource = "Dethrone",
+      Effects = [new DethroneEffect()],
+      Reminder = reminder,
+    }
+  );
+}
